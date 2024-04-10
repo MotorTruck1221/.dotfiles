fe9f8b1b61158fecb1be04fc005b1af350e0f8a3
MotorTruck1221 <motortruck1221@protonmail.com>
2024-04-10 15:33:25 -0600

    Remove --noconfirm

--- setup.sh
+++ setup.sh
@@ -15,16 +15,9 @@ yay -Syyu
 
 # Enabling multilib, community, ungoogled-chromium and arco linux
 echo "Enabling multilib, community, ungoogled-chromium and others..."
-sudo sed -i "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf
-sudo sed -i "/\[community\]/,/Include/"'s/^#//' /etc/pacman.conf
-sudo sed -i "/\[home_ungoogled_chromium_Arch]\nServer = https:\/\/download.opensuse.org\/repositories\/home:/ungoogled_chromium\/Arch\//d" /etc/pacman.conf
-sudo sed -i "/\[arcolinux_repo\]/,/Include/"'s/^#//' /etc/pacman.conf
-sudo sed -i "/\[arcolinux_repo_3party\]/,/Include/"'s/^#//' /etc/pacman.conf
-sudo sed -i "/\[arcolinux_repo_xlarge\]/,/Include/"'s/^#//' /etc/pacman.conf
-sudo sed -i "/\[motortruck1221os-core-repo\]\nSigLevel = Optional DatabaseOptional\nServer = https:\/\/gitlab.com\/MotorTruck1221OS\/\$repo\/-\/raw\/main\/\$arch/d" /etc/pacman.conf
-
+sudo cat ./extraRepos.txt >> /etc/pacman.conf
 # Install packages
 echo "Installing packages..."
 sudo pacman -Syy
-yay -Syy --noconfirm - < pkglist.txt
+yay -Syy - < pkglist.txt
 
